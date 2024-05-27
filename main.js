53482c5ad1f54f081699f1ac9c5cfab5b383f38f
kentem-sh-maehata <sh-maehata@kentem@.co.jp>
2024-05-27 15:55:17 +0900

    完成

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ var _a, _b, _c;
 {
     var thumbnails_1 = document.querySelectorAll('.thumbnail');
     var mainImage_1 = document.getElementById('main-image');
+    var next = document.getElementById('next');
+    var prev = document.getElementById('prev');
+    var activeIndex_1 = 0;
+    function updateThumbnail() {
+        if (activeIndex_1 === 3)
+            activeIndex_1 = 2;
+        if (activeIndex_1 === -1)
+            activeIndex_1 = 0;
+        thumbnails_1.forEach(function (thumbnail) {
+            thumbnail.classList.remove('active');
+        });
+        thumbnails_1[activeIndex_1].classList.add('active');
+        var thumbnail = thumbnails_1[activeIndex_1];
+        if (mainImage_1 && 'src' in mainImage_1 && 'src' in thumbnail) {
+            mainImage_1.src = thumbnail.src;
+        }
+    }
     (_a = thumbnails_1[0]) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
         if (mainImage_1 && 'src' in mainImage_1 && 'src' in thumbnails_1[0])
             mainImage_1.src = thumbnails_1[0].src;
@@ -22,5 +39,13 @@ var _a, _b, _c;
         thumbnails_1[0].classList.remove('active');
         thumbnails_1[1].classList.remove('active');
         thumbnails_1[2].classList.add('active');
+    });
+    next === null || next === void 0 ? void 0 : next.addEventListener('click', function () {
+        activeIndex_1++;
+        updateThumbnail();
+    });
+    prev === null || prev === void 0 ? void 0 : prev.addEventListener('click', function () {
+        activeIndex_1--;
+        updateThumbnail();
     });
 }
--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 {
   const thumbnails = document.querySelectorAll('.thumbnail')
   const mainImage = document.getElementById('main-image')
+  const next = document.getElementById('next')
+  const prev = document.getElementById('prev')
+  let activeIndex:number = 0
 
-  
+  function updateThumbnail(){
+    if(activeIndex === 3)
+      activeIndex = 2
+    if(activeIndex === -1)
+      activeIndex = 0
+    thumbnails.forEach((thumbnail)=>{
+      thumbnail.classList.remove('active')
+    })
+    thumbnails[activeIndex].classList.add('active')
+    const thumbnail = thumbnails[activeIndex]
+    if(mainImage && 'src' in mainImage && 'src' in thumbnail){
+      mainImage.src = thumbnail.src
+    }
+  }
 
   thumbnails[0]?.addEventListener('click',()=>{
     if(mainImage && 'src' in mainImage && 'src' in thumbnails[0])
@@ -27,5 +43,14 @@
     thumbnails[0].classList.remove('active')
     thumbnails[1].classList.remove('active')
     thumbnails[2].classList.add('active')
+  })
+
+  next?.addEventListener('click',()=>{
+    activeIndex ++
+    updateThumbnail()
+  })
+  prev?.addEventListener('click',()=>{
+    activeIndex --
+    updateThumbnail()
   })
 }
